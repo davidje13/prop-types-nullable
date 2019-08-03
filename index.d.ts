2abafaddf481630db3aca50523ec838f68d1867f
David Evans <davidje13@users.noreply.github.com>
2019-08-03 16:01:23 +0100

    Add typescript types

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import { Validator } from 'prop-types';
+
+interface NullableRequireable<T> extends Validator<T | null> {
+  isRequired: Validator<Exclude<T | null, undefined>>;
+}
+
+export default function<T>(
+  type: Validator<T>,
+): NullableRequireable<T | undefined>;
